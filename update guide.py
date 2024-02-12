7b65f67c652414ef09dced1d13b5b2adbf985661
cfailde <carlosfailde@gmail.com>
2024-02-12 11:27:54 +0000

    Additional content and proofreading. Reorganised updater.

--- update guide.py
+++ update guide.py
@@ -10,7 +10,6 @@ from bs4 import BeautifulSoup
 import re
 import json
 import sys
- 
 import os
 import shutil
 from datetime import datetime
@@ -19,106 +18,103 @@ from datetime import datetime
 from yattag import indent as yattag_indent
 
 
+BOTC_DATA_FILE = "BotC.txt"
+RESULT_FILE = "BotC Guide.html"
+DEBUG_MODE = False
+MAX_BACKUPS = 5
+
+
+def debug(data, output_path):
+
+    if DEBUG_MODE:
+        print("  - Saving interim results to " + output_path)
+        with open(output_path, 'w', encoding="utf-8") as file:
+            file.writelines(data)
+
+
+def sanity_check():
+    for f in [BOTC_DATA_FILE, RESULT_FILE]:
+        
+        if os.path.exists(f):
+            print(f"  {f} exists.")
+        else:
+            print(f"  File not found: {f}")
+            return False
+    return True
+
 
 def check_file_format(file_path):
-    try:
-        with open(file_path, 'r', encoding="utf-8") as file:
-            lines = file.readlines()
+    with open(file_path, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
 
-            # Extract file name from the path
-            file_name = file_path.split("/")[-1]
+        # Extract file name from the path
+        file_name = file_path.split("/")[-1]
 
-            # Initialize an empty string to store first letters
-            first_letters = ""
+        # Initialize an empty string to store first letters
+        first_letters = ""
 
-            for line_number, line in enumerate(lines, start=1):
+        for line_number, line in enumerate(lines, start=1):
 
-                # Check if the line is not empty
-                if line:
-                    first_char = line[0]
+            # Check if the line is not empty
+            if line:
+                first_char = line[0]
 
-                    # Skip lines that don't start with Q or A
-                    if first_char not in {'Q', 'A'}:
-                        continue
+                # Skip lines that don't start with Q or A
+                if first_char not in {'Q', 'A'}:
+                    continue
 
-                    # Check for incorrect formatting
-                    if first_char == 'A' and 'Q' not in first_letters:
-                        print(f"{file_name} is incorrectly formatted: 'A' detected before 'Q' on line {line_number}.")
-                        return False
-                    elif first_letters and first_char == first_letters[-1]:
-                        print(f"{file_name} is incorrectly formatted: '{first_char}' detected after another '{first_char}' on line {line_number}.")
-                        return False
+                # Check for incorrect formatting
+                if first_char == 'A' and 'Q' not in first_letters:
+                    print(f"  {file_name} is incorrectly formatted: 'A' detected before 'Q' on line {line_number}.")
+                    return False
+                elif first_letters and first_char == first_letters[-1]:
+                    print(f"  {file_name} is incorrectly formatted: '{first_char}' detected after another '{first_char}' on line {line_number}.")
+                    return False
 
-                    # Add the first character to the string
-                    first_letters += first_char
+                # Add the first character to the string
+                first_letters += first_char
 
-            # Check if the last letter is 'A'
-            if first_letters and first_letters[-1] == 'A':
-                print(f"{file_name} is correctly formatted.")
-                return True
-            else:
-                print(f"{file_name} is incorrectly formatted: Missing 'A' at the end.")
-                return False
-
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-        return False
+        # Check if the last letter is 'A'
+        if first_letters and first_letters[-1] == 'A':
+            print(f"  {file_name} is correctly formatted.")
+            return True
+        else:
+            print(f"  {file_name} is incorrectly formatted: Missing 'A' at the end.")
+            return False
 
 
 def manage_backups(filename):
-    # Define the original and backup file names
-    #html_file = "foo.html"
-    max_backups = 5
+    backup_files = [f for f in os.listdir() if f.startswith(f"Back up of {filename.split('.')[0]}")]
 
-    # Check if foo.html exists
-    if os.path.exists(filename):
-        print(f"{filename} exists.")
+    if len(backup_files) >= MAX_BACKUPS:
+        backup_files.sort(key=lambda x: os.path.getctime(x))
+        oldest_backup = backup_files[0]
+        os.remove(oldest_backup)
+        print(f"  Deleted the oldest backup file: {oldest_backup}")
 
-        # Get the list of existing backup files
-        backup_files = [f for f in os.listdir() if f.startswith(f"Back up of {filename.split('.')[0]}")]
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    backup_filename = f"Back up of {filename.split('.')[0]}-{timestamp}.html"
 
-        # Check the number of existing backup files
-        if len(backup_files) >= max_backups:
-            # Sort backup files by creation time and delete the oldest one
-            backup_files.sort(key=lambda x: os.path.getctime(x))
-            oldest_backup = backup_files[0]
-            os.remove(oldest_backup)
-            print(f"Deleted the oldest backup file: {oldest_backup}")
-
-        # Create a new backup filename
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        backup_filename = f"Back up of {filename.split('.')[0]}-{timestamp}.html"
+    shutil.copy(filename, backup_filename)
+    print(f"  Created backup: {backup_filename}")
 
 
-
-        # Make a copy of foo.html with the new backup filename
-        shutil.copy(filename, backup_filename)
-        print(f"Created backup: {backup_filename}")
-
-    else:
-        print(f"{filename} does not exist.")
-
-
-
-# Surround keywords with <span class='character'></span>
 def surround_keywords_with_span(html_content, keywords, character_type):
     soup = BeautifulSoup(html_content, 'html.parser')
 
     for keyword in keywords:
-        print("Highlighting: " + keyword + "                               ", end='\r')
-        # Find all occurrences of the keyword in the HTML content
-        matches = soup.find_all(string=re.compile(r'\b' + re.escape(keyword) + r'\b'))
+        print("  Highlighting: " + keyword + "                               ", end='\r')
 
-        # Loop through each match and surround with <span class='character'>
+        matches = soup.find_all(string=re.compile(r'\b' + re.escape(keyword) + r'\b'))
         for match in matches:
             content = str(match)
             replaced_content = re.sub(r'\b' + re.escape(keyword) + r'\b', f'<span class="{character_type}">{keyword}</span>', content)
-            #print(replaced_content)
             match.replace_with(BeautifulSoup(replaced_content, 'html.parser'))
+
     return str(soup)
 
 
-def botc_to_nodes(input_path, output_path):
+def text_to_nodes(input_path, output_path):
     output_lines = []
 
     answer_mode = False
@@ -147,9 +143,8 @@ def botc_to_nodes(input_path, output_path):
                     output_lines.append('        </div>\n')   # close paragraph
                     output_lines.append('        <div>')      # open paragraph
 
-
-            # question time (or pair or jinx or hate jinx for now)
-            if line.startswith('Q ') or line.startswith('P ') or line.startswith('H ') or line.startswith('J '):
+            # question time
+            if line.startswith('Q '):
                 if first_question:
                     first_question = False
                 else:
@@ -162,26 +157,12 @@ def botc_to_nodes(input_path, output_path):
                 id += 1
 
                 output_lines.append('    <div class="node">\n')  # open node
-                if line.startswith('Q '):
-                    line = '      <div class="question">' + line[2:]   # open question
-                elif line.startswith('P '):
-                    line = '      <div class="question interesting-interaction">' + line[2:] # open interaction
-                elif line.startswith('H '):
-                    line = '      <div class="question hate-jinx">' + line[2:] # open interaction
-                else:
-                    line = '      <div class="question jinx">' + line[2:] # open interaction
+                line = '      <div class="question">' + line[2:]   # open question
+ 
             elif line.startswith('A '):
                 answer_mode = True
                 output_lines.append('      </div>\n')              # close question
                 line = '      <div class="answer">\n        <div>' + line[2:]     # open answer and open first paragraph
-            elif line.startswith('I '):
-                answer_mode = True
-                output_lines.append('      </div>\n')               # close interaction 
-                line = '      <div class="answer interaction-description">\n        <div>' + line[2:]  # open description and first paragraph
-            elif line.startswith('D '):
-                answer_mode = True
-                output_lines.append('      </div>\n')               # close interaction 
-                line = '      <div class="answer jinx-description">\n        <div>' + line[2:]  # open description and first paragraph
 
             # skip comments
             elif line.startswith('=') or line.startswith('--') or line.startswith(':'):
@@ -210,10 +191,7 @@ def botc_to_nodes(input_path, output_path):
                 output_lines.append(line.replace("*","",1)+"<br>")
                 
             # handle paragraphing within lists
-            elif in_list:
-                output_lines.append(line)
-                output_lines.append("<br>")
-            elif in_ordered_list:
+            elif in_list or in_ordered_list:
                 output_lines.append(line)
                 output_lines.append("<br>")
 
@@ -221,64 +199,35 @@ def botc_to_nodes(input_path, output_path):
                 if not in_ordered_list:
                     output_lines.append(line)
 
-    print("Processed " + str(id) + " nodes")
-    print("Cleaning up blank lines")
-    cleaned_lines = []
-    prev_line_empty = False
-    for line in output_lines:
-        if line.strip() == '':
-            if not prev_line_empty:
-                cleaned_lines.append(line)
-            prev_line_empty = True
-        else:
-            cleaned_lines.append(line)
-            prev_line_empty = False
-
-    output_lines = []
-    i = 0
-    while i < len(cleaned_lines):
-        if cleaned_lines[i].strip() == '' and i + 1 < len(cleaned_lines) and cleaned_lines[i + 1].strip() == '</div>':
-            i += 0
-        else:
-            output_lines.append(cleaned_lines[i])
-        i += 1
-
-
-    with open(output_path, 'w', encoding="utf-8") as file:
-        file.writelines(output_lines)
+    print("  Processed " + str(id) + " nodes")
+    debug(output_lines, output_path)
+    return '\n'.join(output_lines)
 
 
 def replace_nodes(original, replacement, output_path):
-    print("Finding existing version of guide")
+    print("  Opening current version of guide")
     with open(original, 'r', encoding="utf-8") as file:
         html_content = file.read()
         soup = BeautifulSoup(html_content, 'html.parser')
         vault = soup.find('div', id='vault')
-    
-    # Read the content of the replacement html
-    with open(replacement, 'r', encoding="utf-8") as file:
-        replacement_content = file.read()
 
     # Parse the replacement content and remove empty paragraphs
-    print("Removing empty divs")
-    replacement_soup = BeautifulSoup(replacement_content, 'html.parser')
+    print("  Removing empty divs")
+    replacement_soup = BeautifulSoup(replacement, 'html.parser')
     for p_tag in replacement_soup.find_all('div'):
         if not p_tag.text.strip():  # Check if the paragraph has only whitespace
             p_tag.extract()  # Remove the empty paragraph
 
-    print("Updating HTML")
+    print("  Replacing nodes")
     if vault:
         vault.clear()
         vault.append(replacement_soup)
 
-    with open(output_path, 'w', encoding="utf-8") as file:
-        file.write(str(soup))
+    debug(str(soup), output_path)
+    return str(soup)
 
-def highlight_roles(input_path, output_path):
-    with open(input_path, 'r', encoding='utf-8') as file:
-        current_contents = file.read()
 
-    # Update the HTML content with keyword replacements
+def highlight_roles(current_contents, output_path):
     current_contents = surround_keywords_with_span(current_contents, Fabled, "Fabled")
     current_contents = surround_keywords_with_span(current_contents, Townsfolk, "Townsfolk")
     current_contents = surround_keywords_with_span(current_contents, Outsider, "Outsider")
@@ -286,14 +235,12 @@ def highlight_roles(input_path, output_path):
     current_contents = surround_keywords_with_span(current_contents, Demon, "Demon")
     current_contents = surround_keywords_with_span(current_contents, Traveller, "Traveller")
 
-    with open(output_path, 'w', encoding='utf-8') as file:
-        file.write(current_contents)
+    debug( current_contents, output_path)
+    return current_contents
 
-def update_index(input_path, output_path):
-    with open(input_path, 'r', encoding='utf-8') as file:
-        html_content = file.read()
 
-    soup = BeautifulSoup(html_content, 'html.parser')
+def update_index(html, output_path):
+    soup = BeautifulSoup(html, 'html.parser')
     script_tag = soup.find('script')
 
     # Extract the JavaScript code from the <script> tag
@@ -304,11 +251,7 @@ def update_index(input_path, output_path):
     end_index = javascript_code.find("};", start_index) + 1
     roles_dict_str = javascript_code[start_index-1:end_index]
 
-
-
     sorted_keywords = {}
-
-
 
     # Sort names by first letter and populate the dictionary
     for name in sorted(all_the_words):
@@ -325,9 +268,7 @@ def update_index(input_path, output_path):
     for key, value in sorted_keywords.items():
         new_keyword_count += len(value)
 
-
-#
-    print(f"Added {new_keyword_count} keywords to index")
+    print(f"  Added {new_keyword_count} keywords to index")
 
     # Print the sorted dictionary
     #for key, value in sorted_keywords.items():
@@ -342,42 +283,64 @@ def update_index(input_path, output_path):
     # Update the <script> tag with the modified JavaScript code
     script_tag.string = javascript_code
 
-    # Write the updated HTML back to the file
-    with open(output_path, 'w', encoding='utf-8') as file:
-        file.write(str(soup))
+    debug(str(soup), output_path)
+    return str(soup)
 
-def indent(input_path, output_path):
-    with open(input_path, 'r', encoding="utf-8") as file:
-        html = file.read()
+
+def indent_keywords(html):
+    updated_content = []
+    found_start = False
+
+    for line in html.split('\n'):
+        if found_start:
+            updated_content.append(" " * 6 + line)
+        else:
+            if line.strip().startswith("var keywords = {"):
+                found_start = True
+            updated_content.append(line)
+        if line.startswith("};"):
+            found_start = False
+    return "\n".join(updated_content)
+
+def indent(html, output_path):
+    #with open(input_path, 'r', encoding="utf-8") as file:
+    #    html = file.read()
 
     nicely_indented = str(yattag_indent(html))
     fixed_spans = nicely_indented.replace("</span><span", "</span> <span")
 
-    with open(output_path, 'w', encoding="utf-8") as file:
-        file.write(fixed_spans)
+    updated_content = indent_keywords(fixed_spans)
 
-def emphasise(input_file, output_file):
-    # Read the HTML file
-    with open(input_file, 'r', encoding='utf-8') as file:
-        html_content = file.read()
+    debug(updated_content, output_path)
+    return updated_content
 
+
+# Replaces underscores around words with <em></em> tag
+def emphasise(html_content, output_path):
     # Define a regex pattern to find words surrounded by underscores
     pattern = re.compile(r'(_)(\w+)(_)')
 
-    # Replace matched patterns with <em> tags and without underscores
     modified_html = pattern.sub(lambda match: f'<em>{match.group(2)}</em>', html_content)
 
-    # Write the modified HTML to the output file
-    with open(output_file, 'w', encoding='utf-8') as output:
-        output.write(modified_html)
+    debug(modified_html, output_path)
+    return modified_html
 
+def remove_blank_lines(html, output_path):
+    lines = html.split("\n")
+    result = []
+    script_encountered = False
+    for line in lines:
+        if not script_encountered:
+            if line.strip():  # Check if line is not blank
+                result.append(line)
+            if line.strip().startswith("<script>"):
+                script_encountered = True
+        else:
+            result.append(line)
 
-
-
-
+    return "\n".join(result)
 
 # List of keywords to highlight
-
 Townsfolk =  [ "Steward", "Knight", "Noble", "Investigator", "Chef"]
 Townsfolk += [ "Washerwoman", "Clockmaker", "Librarian", "Grandmother"]
 Townsfolk += [ "Pixie", "Bounty Hunter", "Empath", "High Priestess", "Sailor"]
@@ -417,8 +380,6 @@ Fabled    += [ "Spirit of Ivory", "Hell's Librarian", "Djinn", "Duchess"]
 Fabled    += [ "Storm Catcher", "Sentinel", "Doomsayer", "Angel", "Buddhist"] 
 Fabled    += [ "Ferryman", "Gardener"] 
 
-
-
 # All the keywords that are not the names of characters
 Extra  = [ "poisoned", "drunk", "townsfolk", "outsider", "fabled", "traveller"]
 Extra += [ "demon", "minion", "droisoned","good","evil","nomination","execution","preached", "protect"]
@@ -427,42 +388,44 @@ Extra += [ "madness", "setup", "alive", "dead", "vote" ]
 all_the_words = Townsfolk + Outsider + Minion + Demon + Traveller + Fabled + Extra
 
 
-
-
-
 print("Starting update.")
 
-print("Checking BotC.txt")
-if not check_file_format('BotC.txt'):
-  sys.exit("..Oh dear!")
+print("Checking for input files")
+if not sanity_check():
+    sys.exit("  Oh dear!")
 
-manage_backups("BotC Guide.html")
+print("Checking " + BOTC_DATA_FILE)
+if not check_file_format(BOTC_DATA_FILE):
+    sys.exit("  Oh dear!")
 
-botc_to_nodes('BotC.txt', 'nodefied content.txt')
-print("Done nodifying text")
+if DEBUG_MODE:
+    print("Backing up ...")
+    manage_backups(RESULT_FILE)
 
-replace_nodes('BotC Guide.html', 'nodefied content.txt', 'nodified content.html')
+print("Nodifying text ...")
+nodes = text_to_nodes(BOTC_DATA_FILE, 'nodefied content.txt')
 
-print("Updating index")
-update_index('nodified content.html', 'updated index.html')
+print("Placing updated nodes in guide ...")
+interim_result = replace_nodes(RESULT_FILE, nodes, 'nodified content.html')
 
-print("Highlighting roles")
-highlight_roles('updated index.html', 'highlighted.html')
-print("Done highlighting.                                                              ")
+print("Highlighting roles ...")
+interim_result = highlight_roles(interim_result, 'highlighted.html')
+print("  Done highlighting.                                                              ")
 
+print("Updating index ...")
+interim_result = update_index(interim_result, 'updated index.html')
 
-
-
+print("Removing excess blank lines ...")
+interim_result = remove_blank_lines(interim_result, "removed_blank_lines.html")
 
 print("Prettifying ...")
-indent('rough.html', 'guide.html')
-
+interim_result = indent(interim_result, 'pretty.html')
 
 print("Adding emphasis ...")
-emphasise("guide.html", "BotC Guide.html")
+interim_result = emphasise(interim_result, "emphasised.html")
 
+print("Saving updated guide ...")
+with open(RESULT_FILE, 'w', encoding='utf-8') as output:
+    output.write(interim_result)
 
 print("Done updating.")
-
-
-
